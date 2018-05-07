9bb18ee69614f23a59514c30bb7c1f6b1c60e09f
Anusha Datar <anushadatar@gmail.com>
2018-05-07 12:59:03 -0400

    manually collected training data? so professional

--- code/a440_train_vector.py
+++ code/a440_train_vector.py
@@ -4,5 +4,6 @@
 labels = ['A Major', 'B Major', 'C Major', 'D Major', 'E Major', 'F Major', 'G Major']
 
 # Vector of major frequencies present in the chord.
-freqs = []
+freqs = [[62, 124, 61], [123, 124, 165], [65, 66, 64], [110, 73, 147], \
+         [62, 61, 63], [312, 208, 27], [52, 51, 48]]
 
